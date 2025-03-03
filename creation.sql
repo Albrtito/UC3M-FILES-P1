2b5066724cb33ed08712e088a72e127474d86eec
tito <alb.pascau@gmail.com>
2025-03-03 17:42:34 +0100

    CHANGE/STATES

--- creation.sql
+++ creation.sql
@@ -177,7 +177,7 @@ drop table bibus_state cascade constraints;
 CREATE TABLE bibus_state(
     bibus_plate CHAR(8) NOT NULL,
     state_date CHAR(22) NOT NULL,
-    state CHAR(20) NOT NULL DEFAULT 'aviable',
+    state CHAR(20) NOT NULL DEFAULT('aviable'),
     assigned_route_id CHAR(5) NOT NULL,
     CONSTRAINT PK_BIBUS_STATE PRIMARY KEY(bibus_plate, state_date),
     CONSTRAINT FK_REFERENCES_STATE_BIBUS FOREIGN KEY(bibus_plate) REFERENCES BIBUS(bibus_plate),
@@ -188,7 +188,7 @@ drop table bibusero_state cascade constraints;
 CREATE TABLE bibusero_state(
     bibusero_passport CHAR(20) NOT NULL,
     state_date CHAR(22) NOT NULL,
-    state CHAR(20) DEFAULT 'aviable',
+    state CHAR(20) DEFAULT('aviable'),
     assigned_route_id CHAR(5) NOT NULL,
     CONSTRAINT PK_BIBUSERO_STATE PRIMARY KEY(bibusero_passport, state_date),
     CONSTRAINT FK_REFERENCES_STATE_BIBUSERO FOREIGN KEY(bibusero_passport) REFERENCES BIBUSERO(bibusero_passport),
