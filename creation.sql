74d6fb88eeeddf07d2de1a0ef17158b750980f30
tito <alb.pascau@gmail.com>
2025-03-03 14:58:43 +0100

    Testing table creation

--- creation.sql
+++ creation.sql
@@ -4,6 +4,6 @@ subject VARCHAR2(100),
 degree VARCHAR2(50),
 credits NUMBER(2) NOT NULL,
 lecturer VARCHAR2(50),
-CONSTRAINT pk_course PRIMARY KEY(subject, degree),CONSTRAINT valid_ttl CHECK (credits>0),
-CONSTRAINT fk_course_degree FOREIGN KEY(degree)REFERENCES TO degree(name)
+CONSTRAINT pk_course PRIMARY KEY(subject, degree),
+CONSTRAINT valid_ttl CHECK (credits>0),
 );
