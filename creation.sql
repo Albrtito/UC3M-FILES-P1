2c55cd9162d12f3add7c588a8c41ad9c8a78d858
tito <alb.pascau@gmail.com>
2025-03-03 16:19:39 +0100

    CHANGE/USERS

--- creation.sql
+++ creation.sql
@@ -83,7 +83,7 @@ CREATE TABLE users(
     user_phone_number CHAR(9) NOT NULL,
     user_sanction CHAR(20),
     user_passport CHAR(20) NOT NULL,
-    CONSTRAINT PK_USER PRIMARY KEY(user_id),
+    CONSTRAINT PK_USER PRIMARY KEY(user_id)
     --CONSTRAINT FK_REFERENCE_MUNICIPALITY FOREGN KEY(user_municipality) REFERENCES MUNICIPALITY(municipality_name)
 );
 
