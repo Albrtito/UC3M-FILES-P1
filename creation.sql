03b94a4fb1bc32b42f54bc20dd08d79c300c3d32
tito <alb.pascau@gmail.com>
2025-03-03 15:33:42 +0100

    ADD/BOOKS

--- creation.sql
+++ creation.sql
@@ -1,9 +1,17 @@
-DROP TABLE course;
-CREATE TABLE Course (
-subject VARCHAR2(100),
-degree VARCHAR2(50),
-credits NUMBER(2) NOT NULL,
-lecturer VARCHAR2(50),
-CONSTRAINT pk_course PRIMARY KEY(subject, degree),
-CONSTRAINT valid_ttl CHECK (credits>0),
+drop table books cascade constraints;
+
+
+CREATE TABLE books(
+    book_title CHAR(200) NOT NULL,
+    book_main_author CHAR(100) NOT NULL,
+    book_publication_country CHAR(50) NOT NULL,
+    book_original_language CHAR(50) NOT NULL,
+    book_publication_date CHAR(12) NOT NULL,
+    book_alternative_titles CHAR(200),
+    book_subject CHAR(200) NOT NULL,
+    book_content_notes VARCHAR2(2500),
+    book_awards CHAR(200),
+    book_author_mention CHAR(200),
+    book_other_authors CHAR(200),
+    CONSTRAINT PK_PRODUCT PRIMARY KEY (book_title,book_main_author)
 );
