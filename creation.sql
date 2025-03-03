490d57d76e885b113c0b840bb64c2d0c2cfb9ed7
tito <alb.pascau@gmail.com>
2025-03-03 16:03:40 +0100

    CHANGE/BOOK_LOANSS

--- creation.sql
+++ creation.sql
@@ -65,7 +65,7 @@ CREATE TABLE book_loans(
     loan_initial_date CHAR(22) NOT NULL,
     loan_entry CHAR(20) NOT NULL,
     loan_user CHAR(10) NOT NULL,
-    loan_final_date CHARR(22),
+    loan_final_date CHAR(22),
     CONSTRAINT PK_BOOK_LOANS PRIMARY KEY(loan_initial_date,loan_entry,loan_user),
     CONSTRAINT FK_REFERENCE_ENTRY FOREIGN KEY(loan_entry) REFERENCES BOOK_ENTRIES(entry_signature),
     --CONSTRAINT FK_REFERENCE_USER FOREIGN KEY(loan_user) REFERENCES USER(user_id)
