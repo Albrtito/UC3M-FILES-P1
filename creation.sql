932efd8bb149f0bae281938ca492e48d039d7701
tito <alb.pascau@gmail.com>
2025-03-03 15:41:54 +0100

    ADD/BOOK_EDITIONS

--- creation.sql
+++ creation.sql
@@ -13,5 +13,30 @@ CREATE TABLE books(
     book_awards CHAR(200),
     book_author_mention CHAR(200),
     book_other_authors CHAR(200),
-    CONSTRAINT PK_PRODUCT PRIMARY KEY (book_title,book_main_author)
+    CONSTRAINT PK_BOOKS PRIMARY KEY (book_title,book_main_author)
 );
+
+
+drop table book_editions cascade constraints;
+
+CREATE TABLE book_editions(
+    edition_national_identifier CHAR(20) NOT NULL,
+    editoon_ISBN CHAR(20) NOT NULL,
+    edition_main_language CHAR(50) NOT NULL,
+    edition_other_language CHAR(50),
+    edition_number CHAR(50) NOT NULL,
+    edition_publisher CHAR(100) NOT NULL,
+    edition_lenght CHAR(50) NOT NULL,
+    edition_series CHAR(50) NOT NULL,
+    edition_legal_deposit CHAR(20) NOT NULL,
+    edition_publication_place CHAR(50) NOT NULL,
+    edition_dimensions CHAR(50) NOT NULL,
+    edition_physical_characteristics CHAR(200),
+    edition_anciliaryMaterials_notes CHAR(200),
+    edition_URL CHAR(200) NOT NULL,
+    edition_book_title CHAR(200) NOT NULL,
+    edition_book_main_author CHAR(100) NOT NULL,
+    CONSTRAINT PK_BOOK_EDITIONS PRIMARY KEY (edition_national_identifier),
+    CONSTRAINT FK_REFERENCEBOOK FOREIGN KEY(edition_book_title, editoin_book_main_author) REFERENCES BOOKS(book_title,book_main_author)
+);
+
